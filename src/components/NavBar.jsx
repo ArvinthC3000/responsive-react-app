2d822fbe4c546b27024ae57e95c43dbc97d6ab5f
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-07-13 21:16:20 +0530

    Nav bar button changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,26 @@
 import { Link } from 'react-router-dom';
 import { FaTypo3, FaTimes, FaBars } from 'react-icons/fa';
 import { useState } from 'react';
+import Button from './Button';
+import './NavBar.css';
 
 const NavBar = () => {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener('resize', showButton);
+
   return (
     <div>
       <nav className='navbar'>
@@ -14,8 +29,40 @@ const NavBar = () => {
             APP <FaTypo3 />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
-            {click ? <FaTimes /> : <FaBars />}
+            {click ? <FaTimes color='#fff' /> : <FaBars color='#fff' />}
           </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/services'
+                className='nav-links'
+                onClick={closeMobileMenu}>
+                Services
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/products'
+                className='nav-links'
+                onClick={closeMobileMenu}>
+                Products
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/sign-up'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}>
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
         </div>
       </nav>
     </div>
