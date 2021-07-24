8a7dc7fe18a030deb782a8dfb752191c3cfb06d2
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-07-24 16:00:24 +0530

    mobile sensitive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,14 @@
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitter,
+  FaTypo3,
+  FaYoutube,
+} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import Button from './Button';
+import './Footer.css';
 
 const Footer = () => {
   return (
@@ -20,6 +30,91 @@ const Footer = () => {
             />
             <Button buttonStyle='btn--outline'>Subscribe</Button>
           </form>
+        </div>
+      </section>
+      <div class='footer-links'>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h2>About Us</h2>
+            <Link to='/sign-up'>How it works</Link>
+            <Link to='/'>Testimonials</Link>
+            <Link to='/'>Careers</Link>
+            <Link to='/'>Investors</Link>
+            <Link to='/'>Terms of Service</Link>
+          </div>
+          <div class='footer-link-items'>
+            <h2>Contact Us</h2>
+            <Link to='/'>Contact</Link>
+            <Link to='/'>Support</Link>
+            <Link to='/'>Destinations</Link>
+            <Link to='/'>Sponsorships</Link>
+          </div>
+        </div>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h2>Videos</h2>
+            <Link to='/'>Submit Video</Link>
+            <Link to='/'>Ambassadors</Link>
+            <Link to='/'>Agency</Link>
+            <Link to='/'>Influencer</Link>
+          </div>
+          <div class='footer-link-items'>
+            <h2>Social Media</h2>
+            <Link to='/'>Instagram</Link>
+            <Link to='/'>Facebook</Link>
+            <Link to='/'>Youtube</Link>
+            <Link to='/'>Twitter</Link>
+          </div>
+        </div>
+      </div>
+      <section className='social-media'>
+        <div className='social-media-wrap'>
+          <div className='footer-logo'>
+            <Link to='/' className='social-logo'>
+              APP
+              <FaTypo3 />
+            </Link>
+          </div>
+          <small className='website-rights'>
+            APP &#169; {new Date().getFullYear()}
+          </small>
+          <div className='social-icons'>
+            <Link
+              className='social-icon-link facebook'
+              to='/'
+              target='_blank'
+              aria-label='Facebook'>
+              <FaFacebookF />
+            </Link>
+            <Link
+              className='social-icon-link instagram'
+              to='/'
+              target='_blank'
+              aria-label='Instagram'>
+              <FaInstagram />
+            </Link>
+            <Link
+              className='social-icon-link youtube'
+              to='/'
+              target='_blank'
+              aria-label='Youtube'>
+              <FaYoutube />
+            </Link>
+            <Link
+              className='social-icon-link twitter'
+              to='/'
+              target='_blank'
+              aria-label='Twitter'>
+              <FaTwitter />
+            </Link>
+            <Link
+              className='social-icon-link linkedin'
+              to='/'
+              target='_blank'
+              aria-label='LinkedIn'>
+              <FaLinkedin />
+            </Link>
+          </div>
         </div>
       </section>
     </div>
